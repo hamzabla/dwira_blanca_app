56dfdd00af808eb92580ef4a05f67422fb386d55
hamzabla <hamzabessa19@gmail.com>
2022-05-02 11:16:49 +0200

    deleted git ignore files from directories where its not needed and modified AppExtension file in twig

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -8,15 +8,7 @@ use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
-    public function getFilters(): array
-    {
-        return [
-            // If your filter generates SAFE HTML, you should add a third
-            // parameter: ['is_safe' => ['html']]
-            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
-            new TwigFilter('filter_name', [$this, 'doSomething']),
-        ];
-    }
+    
 
     public function getFunctions(): array
     {
